4b122a59f9ebcba0d74220c68b034b13eb9ea36c
Jonathan Liu <jonathanliu03198@gmail.com>
2025-07-18 12:52:04 +0800

    Refactoring App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Layout from './components/common/DefaultLayout';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
@@ -9,6 +9,25 @@ import ContractList from './components/contracts/ContractList';
 import ContractForm from './components/contracts/ContractForm';
 import Dashboard from './components/dashboard/Dashboard';
 import Projects from './components/projects/Projects';
+import NotificationReview from './components/contracts/NotificationReview';
+
+// A simple component to handle routes that require authentication
+const PrivateRoute = ({ children, token, logout, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        token ? (
+          <Layout token={token} logout={logout}>
+            {children}
+          </Layout>
+        ) : (
+          <Redirect to={{ pathname: '/login', state: { from: location } }} />
+        )
+      }
+    />
+  );
+};
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem('token') || '');
@@ -20,65 +39,52 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/login">
-            <Layout token={token} logout={logout} layoutType="full">
-              <Login setToken={setToken} />
-            </Layout>
-          </Route>
-          <Route path="/register">
-            <Layout token={token} logout={logout} layoutType="full">
-              <Register />
-            </Layout>
-          </Route>
-          <Route path="/clients/new">
-            <Layout token={token} logout={logout}>
-              <ClientForm token={token} />
-            </Layout>
-          </Route>
-          <Route path="/clients/:id/edit">
-            <Layout token={token} logout={logout}>
-              <ClientForm token={token} />
-            </Layout>
-          </Route>
-          <Route path="/clients">
-            <Layout token={token} logout={logout}>
-              <ClientList token={token} />
-            </Layout>
-          </Route>
-          <Route path="/contracts/new">
-            <Layout token={token} logout={logout}>
-              <ContractForm token={token} defaultType="new" />
-            </Layout>
-          </Route>
-          <Route path="/contracts/renew">
-            <Layout token={token} logout={logout}>
-              <ContractForm token={token} defaultType="renew" />
-            </Layout>
-          </Route>
-          <Route path="/contracts/:contract_id/edit">
-            <Layout token={token} logout={logout}>
-              <ContractForm token={token} />
-            </Layout>
-          </Route>
-          <Route path="/contracts">
-            <Layout token={token} logout={logout}>
-              <ContractList token={token} />
-            </Layout>
-          </Route>
-          <Route path="/projects">
-            <Layout token={token} logout={logout}>
-              <Projects token={token} />
-            </Layout>
-          </Route>
-          <Route path="/">
-            <Layout token={token} logout={logout}>
-              <Dashboard token={token} />
-            </Layout>
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        {/* Public routes that don't need the main layout */}
+        <Route path="/login">
+          <Login setToken={setToken} />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+
+        {/* --- All Authenticated Routes Go Here --- */}
+        <PrivateRoute path="/dashboard" token={token} logout={logout}>
+          <Dashboard token={token} />
+        </PrivateRoute>
+        <PrivateRoute path="/clients/new" token={token} logout={logout}>
+          <ClientForm token={token} />
+        </PrivateRoute>
+        <PrivateRoute path="/clients/:id/edit" token={token} logout={logout}>
+          <ClientForm token={token} />
+        </PrivateRoute>
+        <PrivateRoute path="/clients" token={token} logout={logout}>
+          <ClientList token={token} />
+        </PrivateRoute>
+        <PrivateRoute path="/contracts/notify" token={token} logout={logout}>
+          <NotificationReview token={token} />
+        </PrivateRoute>
+        <PrivateRoute path="/contracts/new" token={token} logout={logout}>
+          <ContractForm token={token} defaultType="new" />
+        </PrivateRoute>
+        <PrivateRoute path="/contracts/renew" token={token} logout={logout}>
+          <ContractForm token={token} defaultType="renew" />
+        </PrivateRoute>
+        <PrivateRoute path="/contracts/:contract_id/edit" token={token} logout={logout}>
+          <ContractForm token={token} />
+        </PrivateRoute>
+        <PrivateRoute path="/contracts" token={token} logout={logout}>
+          <ContractList token={token} />
+        </PrivateRoute>
+        <PrivateRoute path="/projects" token={token} logout={logout}>
+          <Projects token={token} />
+        </PrivateRoute>
+
+        {/* Redirect root path to the dashboard */}
+        <Route path="/">
+          {token ? <Redirect to="/dashboard" /> : <Redirect to="/login" />}
+        </Route>
+      </Switch>
     </Router>
   );
 }
